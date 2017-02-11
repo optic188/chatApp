1a98e49dc8024bfc22b80eeed19b6dc2c784abdf
oleksandr sazonov <optic18@gmail.com>
2017-02-11 15:35:42 +0200

    change root folder name

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 // http://webpack.github.io/docs/configuration.html
 // http://webpack.github.io/docs/webpack-dev-server.html
-var app_root = 'src_users'; // the app root folder: src, src_users, etc
+var app_root = 'App'; // the app root folder: src, App, etc
 var path = require('path');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  app_root: app_root, // the app root folder, needed by the other webpack configs
+  app_root: 'app_root', // the app root folder, needed by the other webpack configs
   entry: [
     // http://gaearon.github.io/react-hot-loader/getstarted/
     'webpack-dev-server/client?http://localhost:8080',
